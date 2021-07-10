e30a9f526c2bbf92d2697605af5fbdf2aff83fc8
Varad Deouskar <52121257+Varadd9406@users.noreply.github.com>
2021-07-10 23:23:48 +0530

    Added Resolve in stencil

--- Project1/Stencil.h
+++ Project1/Stencil.h
@@ -13,12 +13,16 @@ namespace bind
 			Mask
 		};
 		Stencil(Graphics& gfx, Mode mode)
+			:
+			mode(mode)
 		{
 			D3D11_DEPTH_STENCIL_DESC dsDesc = CD3D11_DEPTH_STENCIL_DESC{ CD3D11_DEFAULT{} };
 
 			if (mode == Mode::Write)
 			{
-				dsDesc.StencilEnable = TRUE;
+				dsDesc.DepthEnable = FALSE;
+				dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
+				dsDesc.StencilEnable = FALSE;
 				dsDesc.StencilWriteMask = 0xFF;
 				dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
 				dsDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_REPLACE;
@@ -26,6 +30,7 @@ namespace bind
 			else if (mode == Mode::Mask)
 			{
 				dsDesc.DepthEnable = FALSE;
+				dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
 				dsDesc.StencilEnable = TRUE;
 				dsDesc.StencilReadMask = 0xFF;
 				dsDesc.FrontFace.StencilFunc = D3D11_COMPARISON_NOT_EQUAL;
@@ -34,14 +39,36 @@ namespace bind
 
 			GetDevice(gfx)->CreateDepthStencilState(&dsDesc, &pStencil);
 		}
-		void Bind(Graphics& gfx) noexcept override
+		void Bind(Graphics& gfx) override
 		{
 			GetContext(gfx)->OMSetDepthStencilState(pStencil.Get(), 0xFF);
 		}
-		//static std::shared_ptr<Stencil> Resolve( Graphics& gfx,bool blending,std::optional<float> factor = {} );
-		//static std::string GenerateUID( bool blending,std::optional<float> factor );
-		//std::string GetUID() const noexcept override;
+		static std::shared_ptr<Stencil> Resolve(Graphics& gfx, Mode mode)
+		{
+			return Codex::Resolve<Stencil>(gfx, mode);
+		}
+		static std::string GenerateUID(Mode mode)
+		{
+			using namespace std::string_literals;
+			const auto modeName = [mode]() {
+				switch (mode) {
+				case Mode::Off:
+					return "off"s;
+				case Mode::Write:
+					return "write"s;
+				case Mode::Mask:
+					return "mask"s;
+				}
+				return "ERROR"s;
+			};
+			return typeid(Stencil).name() + "#"s + modeName();
+		}
+		std::string GetUID() const override
+		{
+			return GenerateUID(mode);
+		}
 	private:
+		Mode mode;
 		Microsoft::WRL::ComPtr<ID3D11DepthStencilState> pStencil;
 	};
 
